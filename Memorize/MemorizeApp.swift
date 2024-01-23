6ad4595350589b9a3eb7fe30e722ac26b3a626b7
Ikramuzzaman Muntasir <30627757+muntasir14@users.noreply.github.com>
2024-01-23 17:59:38 +0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ikramuzzaman Muntasir on 23/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
